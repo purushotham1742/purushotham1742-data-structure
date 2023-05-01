f046cfd02d6523df4390b4b4db4997965cce239f
purushotham1742 <132130572+purushotham1742@users.noreply.github.com>
2023-05-01 16:52:10 +0530

    Create Question 2 Greatest of two numbers.c

---
+++ Question 2 Greatest of two numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main ()
+{
+  int num1, num2;
+  num1=12,num2=13;
+
+  if (num1 == num2)
+    printf("both are equal");
+  else if (num1 > num2) 
+    printf("%d is greater", num1);
+  else
+    printf("%d is greater", num2);
+
+  return 0;
+}
